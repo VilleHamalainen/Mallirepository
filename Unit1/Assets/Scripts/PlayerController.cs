bf223e0d31f598de08fd41c3602f7c14918a1c3a
VilleHamalainen <33627661+VilleHamalainen@users.noreply.github.com>
2020-05-07 13:42:59 +0300

    Unity tehtava 1

---
+++ Unit1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float horizontalInput;
+    private float forwardInput;
+    private float speed = 20f;
+    private float turnSpeed = 45f;
+    void Start()
+    {
+//        Debug.Log("login testaaminen");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        //moves car "forward" on Z-axis
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //turns cas in X-asix
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        //this.transform.Rotate()
+        
+        MoveCar();
+    }
+
+    private static void MoveCar()
+    {
+      // move the vehicle
+    }
+}
